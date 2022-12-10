2c93376fac6b2ce88173fc15bcf0304c2f43b671
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-10 20:13:16 +0100

    created the main "app" server 🎖

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan"
+
+
+const app = express()
+
+app.use(express.json())
+app.use(logger("dev"))
+app.use(cookieParser())
+
+
+
+const port = 3700
+app.listen(port, ()=>{
+  console.log(`Server is running on port:${port}`)
+})
+
+export default app
