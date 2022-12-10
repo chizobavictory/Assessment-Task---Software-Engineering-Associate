36a8ee3087008e8b85582c9f967faaa5ff0a2795
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-10 21:20:28 +0100

    Added http-authentication 🔐

---
+++ src/controller/userController.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from 'express'
+
+
+//create user controller with http authentication
---
+++ src/middleware/auth.ts
@@ -0,0 +1,18 @@
+import base64 from 'base-64';
+import { Request, Response, NextFunction } from 'express';
+
+
+export default function auth(req: Request, res: Response, next: NextFunction){
+  const authHeader = req.headers.authorization || ''
+  const [username, password] = base64.decode(authHeader.split(' ')[1]).split(':')
+  if(username === 'admin' && password === 'admin'){
+    next()
+}
+
+res.set('WWW-Authenticate', 'Basic realm="user"')
+res.status(401).send('Authentication required')
+}
+
+  
+
+
