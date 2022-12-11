864f23ea3e4809bcbc989edde5e9e9304877c923
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-11 01:50:45 +0100

    changed the create user controller function 🎮

--- dist/controller/userController.js
+++ dist/controller/userController.js
@@ -7,8 +7,20 @@ exports.createUser = void 0;
 const User_1 = __importDefault(require("../model/User"));
 //create user
 const createUser = async (req, res) => {
-    const { firstname, lastname, gender, date_of_birth } = req.body;
-    const user = await User_1.default.create({ firstname, lastname, gender, date_of_birth });
-    res.status(201).json(user);
+    // const { firstname, lastname, gender, date_of_birth } = req.body
+    // const user = await User.create({ firstname, lastname, gender, date_of_birth })
+    const newUser = new User_1.default({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        gender: req.body.gender,
+        date_of_birth: req.body.date_of_birth,
+    });
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
 };
 exports.createUser = createUser;
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -4,8 +4,19 @@ import User from '../model/User'
 
 //create user
 export const createUser = async (req: Request, res: Response) => {
-  const { firstname, lastname, gender, date_of_birth } = req.body
-  const user = await User.create({ firstname, lastname, gender, date_of_birth })
-  res.status(201).json(user)
+  // const { firstname, lastname, gender, date_of_birth } = req.body
+  // const user = await User.create({ firstname, lastname, gender, date_of_birth })
+  const newUser = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    gender: req.body.gender,
+    date_of_birth: req.body.date_of_birth,
+  })
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 } 
 
